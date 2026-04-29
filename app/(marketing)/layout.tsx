24457dd2209d449b202eb4a3054aaeca2bacfa66
Attila0312 <bakosattilaev@gmail.com>
2026-04-29 20:43:04 +0200

    feat: matrix background with click ripple + console fixes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import type { ReactNode } from "react";
+import dynamic from "next/dynamic";
 
 import { SmoothScroll } from "@/components/animations/smooth-scroll";
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/header";
+
+const MatrixBackground = dynamic(
+  () => import("@/components/ui/MatrixBackground").then(mod => mod.MatrixBackground),
+  { ssr: false }
+);
 
 export default function MarketingLayout({
   children,
@@ -12,8 +20,9 @@ export default function MarketingLayout({
   return (
     <SmoothScroll>
       <div className="flex min-h-screen flex-col">
+        <MatrixBackground />
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="relative z-10 flex-1">{children}</main>
         <Footer />
       </div>
     </SmoothScroll>
